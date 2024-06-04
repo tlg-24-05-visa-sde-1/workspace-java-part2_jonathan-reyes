519bcab17116e5b03486c81f80951068c069dcdd
Jonathan Reyes <jonathanr@tlgcohort.com>
2024-06-04 13:43:26 -0500

    Iteration 1: DuckRacer and Reward

---
+++ DuckRace/src/com/duckrace/DuckRacer.java
@@ -0,0 +1,49 @@
+package com.duckrace;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+class DuckRacer {
+    //properties, fields, instance variables
+    private final int id;
+    private String name;
+    private final Collection<Reward> rewards = new ArrayList<>();
+
+    //constructors
+    public DuckRacer(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void win(Reward reward){
+        rewards.add(reward);
+    }
+
+    //accessor methods
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //Derived property, don't need to keep track of the wins as you can see the amount through the rewards list
+    public int getWins(){
+        return rewards.size();
+    }
+
+    public Collection<Reward> getRewards() {
+        return Collections.unmodifiableCollection(rewards);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s: id=%s, name=%s, wins=%s, rewards=%s",getClass().getSimpleName(),getId(),getName(),getWins(),getRewards());
+    }
+}
