0ba262369cb4ebe72bf369b2bb0bfed4f969ad64
Jonathan Reyes <jonathanr@tlgcohort.com>
2024-06-12 14:54:03 -0500

    Named inner class DummyEmployee or anonymous class for mocking Employee, for equals testing

--- Lab03.2/test/com/javatunes/personnel/EmployeeTest.java
+++ Lab03.2/test/com/javatunes/personnel/EmployeeTest.java
@@ -14,8 +14,27 @@ public class EmployeeTest {
 
     @Before
     public void setUp() {
-        emp1 = new SalariedEmployee("John", Date.valueOf("2000-01-01"));
-        emp2 = new SalariedEmployee("John", Date.valueOf("2000-01-01"));
+        // create two instances of your named inner class
+       // emp1 = new DummyEmployee("John", Date.valueOf("2000-01-01"));
+       // emp2 = new DummyEmployee("John", Date.valueOf("2000-01-01"));
+
+        //create instances of an anonymous class instead, But Redundancy happens here => create a proper
+        //class definition - in this case the named member inner class ( in this case the DummyEmployee class you can see below)
+        emp1 = getEmployee();
+        emp2 = getEmployee();
+
+    }
+
+    //mock
+    private static Employee getEmployee() {
+        return new Employee("John", Date.valueOf("2000-01-01")) {
+            public double pay() {
+                return 0;
+            }
+            public double payTaxes() {
+                return 0;
+            }
+        };
     }
 
     @Test
@@ -38,5 +57,26 @@ public class EmployeeTest {
     public void equals_shouldReturnTrue_allPropertiesSame() {
         assertEquals(emp1,emp2);                //this does an .equals() check
         assertTrue(emp1.equals(emp2));          //alternative assertion
+    }
+
+    //Named Member level Inner Classes
+    private class DummyEmployee extends Employee {
+
+        public DummyEmployee(String name, Date hireDate) {
+            super(name, hireDate);
+        }
+
+        @Override
+        public double pay() {
+            return 0;
+        }
+
+        public DummyEmployee() {
+        }
+
+        @Override
+        public double payTaxes() {
+            return 0;
+        }
     }
 }
